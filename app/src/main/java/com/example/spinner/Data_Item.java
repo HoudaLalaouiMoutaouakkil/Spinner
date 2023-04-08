dc9cd38dc3cfa058313fa449509580231e499f6b
HOUDA LALAOUI MOUTAOUAKKIL <houdalalaoui115@gmail.com>
2023-04-08 00:59:47 +0000

    Spinner

---
+++ app/src/main/java/com/example/spinner/Data_Item.java
@@ -0,0 +1,13 @@
+package com.example.spinner;
+
+public class Data_Item {
+    public String name;
+    public int image;
+    public String Details;
+
+    public Data_Item(String name, int image, String details) {
+        this.name = name;
+        this.image = image;
+        Details = details;
+    }
+}
